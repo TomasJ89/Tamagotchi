b1da4ed3a1f8461c07e4d9b35ffb2bd7b0e00532
unknown <tomasjarutis@gmail.com>
2024-05-20 16:03:25 +0300

    bandau

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const pointsHTML = document.querySelector("h3");
 const playingContainer = document.querySelector(".playingContainer");
 const foodItems = document.querySelector(".foodItems");
 const levelHtml = document.querySelector("h4");
+const spriteDiv = document.querySelector(".sprite");
 const animals = ["🐱", "🐭", "🐰", "🐶", "🐯", "🐮", "🐷", "🐵"];
 const bigAnimals = ["🐈", "🐀", "🐇", "🐕", "🐅", "🐄", "🐖", "🐒"];
 const food = ["🥩", "🍗", "🍖", "🦴", "🌭", "🍔"];
@@ -32,6 +33,15 @@ let points = 0;
 let gameTrigger = false;
 let level = 0;
 let petSize = 50;
+let position = 0;
+let position1 = 0;
+let startPosition = 100;
+let backgroundSize = 400;
+let widthHeight = 100;
+let firstMove = 300;
+let secondMove = 600;
+let thirdMove = 900;
+let lastMove = 1200;
 animals.forEach((animal, index) => {
     const animalDiv = document.createElement('div');
     animalDiv.classList.add('box');
@@ -61,20 +71,57 @@ playButton.onclick = () => {
         hungryInterval();
         chooseAnimal.classList.add("d-none");
         const pet = document.createElement("div");
-        pet.className = "pet";
-        pet.innerHTML = selectedAnimal[0].innerHTML;
+        pet.className = "sprite";
+        pet.classList.add("pet");
         walkingPlace.appendChild(pet);
+        const spriteDiv = document.querySelector(".sprite");
+        setInterval(() => {
+            spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`;
+            position += startPosition;
+            if (position > firstMove) {
+                spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`;
+                position1 = startPosition;
+            }
+            if (position > secondMove) {
+                spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`;
+                position1 += startPosition;
+            }
+            if (position > thirdMove) {
+                spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`;
+                position1 += startPosition;
+            }
+            if (position > lastMove) {
+                position = 0;
+                position1 = 0;
+            }
+        }, 300);
+        function growing() {
+            startPosition *= 2;
+            backgroundSize *= 2;
+            widthHeight *= 2;
+            firstMove *= 2;
+            secondMove *= 2;
+            thirdMove *= 2;
+            lastMove *= 2;
+            spriteDiv.style.backgroundSize = `${backgroundSize}px ${backgroundSize}px`;
+            spriteDiv.style.height = `${widthHeight}px`;
+            spriteDiv.style.width = `${widthHeight}px`;
+        }
         const levelInterval = setInterval(() => {
             level++;
             levelHtml.textContent = `Level: ${level}`;
-            petSize += 25;
-            pet.style.fontSize = `${petSize}px`;
-            if (level === 4) {
-                const biggerPet = selectedAnimal[0].id;
-                pet.style.fontSize = "200px";
-                pet.innerHTML = bigAnimals[biggerPet];
+            // petSize+=25
+            // pet.style.fontSize= `${petSize}px`
+            if (level === 5) {
+                growing();
             }
-        }, 1000);
+            if (level === 10) {
+                growing();
+            }
+        }, 5000);
+        if (health <= 0) {
+            clearInterval(levelInterval);
+        }
     }
     else {
         alert("You must choose the animal!");
@@ -85,8 +132,9 @@ function updateBars() {
     hungryBar.style.width = `${hungry}%`;
     funBar.style.width = `${fun}%`;
 }
+let mainInterval = 0;
 function funInterval() {
-    const mainInterval = setInterval(() => {
+    mainInterval = setInterval(() => {
         fun -= 5;
         updateBars();
         if (fun <= 0) {
@@ -106,6 +154,10 @@ function hungryInterval() {
     }, 1000);
 }
 function healthInterval() {
+    if (fun === 100 && hungry === 100) {
+        health = 100;
+        updateBars();
+    }
     if (hungry <= 0 || fun <= 0) {
         const healthInterval = setInterval(() => {
             health--;
@@ -117,17 +169,22 @@ function healthInterval() {
         }, 1000);
     }
 }
-updateBars(); // Initial call to set bars to starting values
+updateBars();
 playWithPetButton.onclick = () => {
+    const petGame = setInterval(turnRandomCellRed, 1000);
     if (!gameTrigger) {
         walkingPlace.classList.add("d-none");
         playingContainer.classList.remove("d-none");
         playWithPetButton.textContent = "Close game";
+        points = 0;
+        pointsHTML.textContent = `Points: ${points}`;
+        clearInterval(mainInterval);
     }
     else {
         walkingPlace.classList.remove("d-none");
         playingContainer.classList.add("d-none");
         playWithPetButton.textContent = "Play with Pet";
+        clearInterval(petGame);
     }
     gameTrigger = !gameTrigger;
     // Clear existing cells
@@ -154,6 +211,18 @@ playWithPetButton.onclick = () => {
                 updateBars();
                 clearInterval(petGame);
                 alert("Your pet is happy 100 % ! ");
+                if (health < 100) {
+                    health += 20;
+                    updateBars();
+                }
+                funInterval();
+                points = 0;
+                pointsHTML.textContent = `Points: ${points}`;
+                walkingPlace.classList.remove("d-none");
+                playingContainer.classList.add("d-none");
+                playWithPetButton.textContent = "Play with Pet";
+                clearInterval(petGame);
+                gameTrigger = !gameTrigger;
             }
         }
     }
@@ -163,7 +232,6 @@ playWithPetButton.onclick = () => {
         div.onclick = handleCellClick;
         gameContainer.appendChild(div);
     });
-    const petGame = setInterval(turnRandomCellRed, 1000);
 };
 feedButton.onclick = () => {
     food.forEach(el => {
@@ -172,6 +240,10 @@ feedButton.onclick = () => {
         item.textContent = el;
         foodItems.appendChild(item);
         item.onclick = () => {
+            if (health < 100) {
+                health++;
+                updateBars();
+            }
             hungry += 20;
             updateBars();
             foodItems.removeChild(item);
--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ const pointsHTML = document.querySelector("h3") as HTMLElement
 const playingContainer = document.querySelector(".playingContainer") as HTMLElement
 const foodItems = document.querySelector(".foodItems") as HTMLElement
 const levelHtml = document.querySelector("h4") as HTMLElement
+const spriteDiv = document.querySelector(".sprite") as HTMLElement
 
 const animals = ["🐱", "🐭", "🐰", "🐶", "🐯", "🐮", "🐷", "🐵"];
 const bigAnimals = ["🐈", "🐀", "🐇", "🐕", "🐅", "🐄", "🐖", "🐒"]
@@ -32,6 +33,17 @@ let points = 0;
 let gameTrigger = false
 let level = 0
 let petSize = 50
+
+let position = 0
+let position1=0
+let startPosition = 100
+let backgroundSize= 400;
+let widthHeight = 100
+
+let firstMove = 300
+let secondMove = 600
+let thirdMove = 900
+let lastMove = 1200
 
 animals.forEach((animal, index) => {
     const animalDiv = document.createElement('div');
@@ -63,20 +75,65 @@ playButton.onclick = () => {
         hungryInterval()
         chooseAnimal.classList.add("d-none")
         const pet = document.createElement("div")
-        pet.className = "pet";
-        pet.innerHTML = selectedAnimal[0].innerHTML
+        pet.className = "sprite";
+        pet.classList.add("pet")
         walkingPlace.appendChild(pet)
+        const spriteDiv = document.querySelector(".sprite") as HTMLElement
+        setInterval(() => {
+            spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`
+            position += startPosition
+            if (position > firstMove) {
+                spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`
+                position1 = startPosition
+            }
+            if (position > secondMove) {
+                spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`
+                position1 += startPosition
+            }
+            if (position > thirdMove) {
+                spriteDiv.style.backgroundPosition = `-${position1}px -${position}px`
+                position1 += startPosition
+            }
+            if (position > lastMove) {
+                position = 0
+                position1 = 0
+
+            }
+
+        },300)
+
+        function growing() {
+            startPosition *= 2
+            backgroundSize *= 2
+            widthHeight *= 2
+            firstMove *= 2
+            secondMove *= 2
+            thirdMove *= 2
+            lastMove *= 2
+            spriteDiv.style.backgroundSize = `${backgroundSize}px ${backgroundSize}px`
+            spriteDiv.style.height = `${widthHeight}px`
+            spriteDiv.style.width = `${widthHeight}px`
+
+        }
+
         const levelInterval = setInterval(() => {
             level++;
             levelHtml.textContent = `Level: ${level}`
-            petSize+=25
-            pet.style.fontSize= `${petSize}px`
-            if (level === 4) {
-                const biggerPet = selectedAnimal[0].id
-                pet.style.fontSize = "200px"
-                pet.innerHTML = bigAnimals[biggerPet]
+            // petSize+=25
+            // pet.style.fontSize= `${petSize}px`
+            if (level === 5) {
+                growing()
+
             }
-        }, 1000);
+            if (level ===10) {
+                growing()
+            }
+
+        }, 5000);
+
+        if (health <= 0) {
+            clearInterval(levelInterval)
+        }
     } else {
         alert("You must choose the animal!")
     }
@@ -87,9 +144,9 @@ function updateBars() {
     hungryBar.style.width = `${hungry}%`
     funBar.style.width = `${fun}%`
 }
-
+let mainInterval = 0
 function funInterval() {
-    const mainInterval = setInterval(() => {
+    mainInterval = setInterval(() => {
         fun -= 5;
         updateBars();
         if (fun <= 0) {
@@ -111,6 +168,10 @@ function hungryInterval() {
 }
 
 function healthInterval() {
+    if (fun === 100 && hungry===100) {
+        health=100
+        updateBars();
+    }
     if (hungry <= 0 || fun <= 0) {
         const healthInterval = setInterval(() => {
             health--;
@@ -124,18 +185,25 @@ function healthInterval() {
     }
 }
 
-updateBars(); // Initial call to set bars to starting values
+updateBars();
 
 playWithPetButton.onclick = () => {
-
+    const petGame = setInterval(turnRandomCellRed, 1000);
     if (!gameTrigger) {
         walkingPlace.classList.add("d-none");
         playingContainer.classList.remove("d-none");
         playWithPetButton.textContent = "Close game"
+        points=0
+        pointsHTML.textContent = `Points: ${points}`;
+        clearInterval(mainInterval)
+
     } else {
         walkingPlace.classList.remove("d-none");
         playingContainer.classList.add("d-none");
         playWithPetButton.textContent = "Play with Pet"
+        clearInterval(petGame)
+
+
     }
     gameTrigger = !gameTrigger
 
@@ -165,6 +233,19 @@ playWithPetButton.onclick = () => {
                 updateBars()
                 clearInterval(petGame)
                 alert("Your pet is happy 100 % ! ")
+                if (health < 100) {
+                    health +=20
+                    updateBars()
+                }
+                funInterval()
+                points=0
+                pointsHTML.textContent = `Points: ${points}`;
+                walkingPlace.classList.remove("d-none");
+                playingContainer.classList.add("d-none");
+                playWithPetButton.textContent = "Play with Pet"
+                clearInterval(petGame)
+                gameTrigger = !gameTrigger
+
             }
         }
     }
@@ -176,7 +257,7 @@ playWithPetButton.onclick = () => {
         gameContainer.appendChild(div);
     });
 
-    const petGame = setInterval(turnRandomCellRed, 1000);
+
 };
 
 feedButton.onclick = () => {
@@ -186,9 +267,14 @@ feedButton.onclick = () => {
         item.textContent = el
         foodItems.appendChild(item)
         item.onclick = () => {
+            if (health < 100) {
+                health++
+                updateBars()
+            }
             hungry += 20
             updateBars()
             foodItems.removeChild(item)
+
         }
     })
 }
